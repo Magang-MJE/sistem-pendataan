da62798dea89cf08a34c631c09021068886787c6
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2023-11-06 21:08:58 +0700

    route

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -7,6 +7,6 @@
   on:click={onclick}
   class="bg-[#00933B] rounded-xl p-3 text-white border-b-2 border-[#006F2C] font-bold"
 >
-  Button
-  <!-- <slot /> -->
+  <!-- Button -->
+  <slot />
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,9 @@
 
 <!-- <Button /> -->
 <div class="mx-auto font-bold text-white text-center my-10">
-  Silahkan masuk melalui QR Code yang disediakan :)
+  <!-- Silahkan masuk melalui QR Code yang disediakan :) -->
+
+  <a href="/hari-1">(hari 1)</a>
+  <a href="/hari-2">(hari 2)</a>
+  <a href="/hari-3">(hari 3)</a>
 </div>
