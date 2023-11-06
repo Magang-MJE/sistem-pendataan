c67656b20f8153f630f0107589a3651eb925c4e7
NoCrypt <57245077+NoCrypt@users.noreply.github.com>
2023-11-06 23:50:53 +0700

    fix immutable on prod

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
   import "../app.postcss";
 </script>
 
-<div class="bg-[url('./bg.webp')] min-h-screen bg-no-repeat bg-cover">
+<div class="bg-[url('/bg.webp')] min-h-screen bg-no-repeat bg-cover">
   <div class="border border-transparent mx-auto max-w-2xl px-5">
-    <img src="./logo.png" alt="logo MJE" class="mx-auto w-44 my-5" />
+    <img src="/logo.png" alt="logo MJE" class="mx-auto w-44 my-5" />
     <slot />
   </div>
 </div>
